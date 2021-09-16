da80ae003b1ed002d5653d13c868174284a81f2d
kubajznicek0 <kubajznicek0@gmail.com>
2021-09-16 09:59:40 +0200

    lepsi vysledek a kontrola operace

--- kalkulacka/pokus.py
+++ kalkulacka/pokus.py
@@ -29,6 +29,15 @@ print('5 odmocnina na 2')
 print('6 umocnění')
 operace = input ("zadejte operaci ")
 
+while True:
+
+    if operace in "1, 2, 3, 4, 5, 6,":
+        break
+    else:
+        print ('zadaji jste neplatný znak' )
+        operace = input('znovu zadejte operaci ')
+
+
 
 cislo1 = float( input("prvni cislo "))
 
@@ -36,35 +45,39 @@ while True:
     if operace == '1':
         cislo2 = float( input("druhe cislo "))
         vysledek = scitani(cislo1, cislo2)
+        operator = "+"
 
     elif operace == '2':
         cislo2 = float( input("druhe cislo "))
         vysledek = odcitani(cislo1, cislo2)
+        operator = "-"
 
     elif operace == '3':
         cislo2 = float( input("druhe cislo "))
         vysledek = nasobeni(cislo1, cislo2)
+        operator = "*"
 
     elif operace == '4':
         cislo2 = float( input("druhe cislo "))
         vysledek = deleni(cislo1, cislo2)
+        operator = "/"
 
     elif operace == '5':
         vysledek = (math.sqrt(cislo1))
+        operator = "odmocneno"
 
     elif operace == '6':
         cislo2 = float( input("druhe cislo "))
         vysledek = umocneni(cislo1, cislo2)
-
-    else:
-        print ('zadaji jste neplatný znak' )
-        operace = input('znovu zadejte operaci ')
+        operator = "umocneno"
     
     if cislo1 != 'nic':
         break
 
 
-print('výsledek je', vysledek)
+# print('výsledek je', vysledek)
+print( )
+print(cislo1, operator, cislo2, "=", vysledek)
 
 #print(len(vysledek))
 
