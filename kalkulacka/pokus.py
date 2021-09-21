77c56909241fcf3c8681b3cf94bacf5a0fcb6d3f
kubajznicek0 <kubajznicek0@gmail.com>
2021-09-21 07:58:03 +0200

    fungcni kontrola cisel

--- kalkulacka/pokus.py
+++ kalkulacka/pokus.py
@@ -1,10 +1,9 @@
 import math
 
-#ahoj more
-
-vysledek = 'nic'
-cislo1 = 'nic'
-cislo2 = 'nic2'
+vysledek = None
+cislo1 = None
+cislo2 = None
+number  = None
 
 def scitani(A, B):
     return A + B
@@ -21,13 +20,30 @@ def deleni(A, B):
 def umocneni(A, B):
     return A ** B
 
+def odmocnovani(A):
+    return (math.sqrt(A))
+
+def MujInput ():
+    while True:
+        try:
+            number = (float( input("zadejte cislo: ")))
+        except ValueError:
+            continue
+        else:
+            print(number)
+            return number
+
+
+#  odstrannit korba.py
+
+
 print('1 sčítání')
 print('2 odčítání')
 print('3 násobení')
 print('4 dělení')
 print('5 odmocnina na 2')
 print('6 umocnění')
-operace = input ("zadejte operaci ")
+operace = input ("zadejte operaci: ")
 
 while True:
 
@@ -35,39 +51,39 @@ while True:
         break
     else:
         print ('zadaji jste neplatný znak' )
-        operace = input('znovu zadejte operaci ')
+        operace = input('znovu zadejte operaci: ')
 
 
-
-cislo1 = float( input("prvni cislo "))
+cislo1 = MujInput()
+# 9print(cislo1)
 
 while True:
     if operace == '1':
-        cislo2 = float( input("druhe cislo "))
+        cislo2 = MujInput()
         vysledek = scitani(cislo1, cislo2)
         operator = "+"
 
     elif operace == '2':
-        cislo2 = float( input("druhe cislo "))
+        cislo2 = MujInput()
         vysledek = odcitani(cislo1, cislo2)
         operator = "-"
 
     elif operace == '3':
-        cislo2 = float( input("druhe cislo "))
+        cislo2 = MujInput()
         vysledek = nasobeni(cislo1, cislo2)
         operator = "*"
 
     elif operace == '4':
-        cislo2 = float( input("druhe cislo "))
+        cislo2 = MujInput()
         vysledek = deleni(cislo1, cislo2)
         operator = "/"
 
     elif operace == '5':
-        vysledek = (math.sqrt(cislo1))
+        vysledek = odmocnovani(cislo1)
         operator = "odmocneno"
 
     elif operace == '6':
-        cislo2 = float( input("druhe cislo "))
+        cislo2 = MujInput()
         vysledek = umocneni(cislo1, cislo2)
         operator = "umocneno"
     
